046d546be13c4fba625ae9eff4c6b22b5c7bb1d5
Dirk Coetzer <dirk.coetzer@viamedia.co.za>
2014-05-08 12:42:30 +0200

    add initial package files.

---
+++ src/Dirkcoetzer/Image/ImageServiceProvider.php
@@ -0,0 +1,53 @@
+<?php namespace Dirkcoetzer\Image;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImageServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('dirkcoetzer/image');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['Image'] = $this->app->share(function($app) { 
+			return new Image; 
+		});
+
+
+		$this->app->booting(function()
+		{
+		  	$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+		  	$loader->alias('Image', 'Dirkcoetzer\Image\Facades\Image');
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('Image');
+	}
+
+}
