c3a24b1da6932e489bc4223305dc5b122a6dfaa4
Dirk Coetzer <dirkcoetzer@gmail.com>
2014-05-08 17:08:39 +0200

    add default config file.

---
+++ src/config/image.php
@@ -0,0 +1,22 @@
+<?php 
+
+return array(
+    'library'     => 'imagick',
+    'upload_path' => public_path() . '/uploads/',
+    'quality'     => 85,
+ 
+    'dimensions' => array(
+        'thumb'  => array(
+        	"width" => 100, 
+        	"height" => 100, 
+        	"crop" => true,  
+        	"quality" => 80
+        ),
+        'medium' => array(
+        	"width" => 600, 
+        	"height" => 400, 
+        	"crop" => false, 
+        	"quality" => 90
+        ),
+    ),
+);
